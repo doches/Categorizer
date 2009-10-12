2195a9372934742f90dac17667b6189ba6370ab4
Trevor Fountain <doches@gmail.com>
2009-10-12 15:43:30 +0100

    Added CW graphs

--- PlotNaming.rb
+++ PlotNaming.rb
@@ -4,7 +4,7 @@ require 'lib/Report'
 cats = {}
 
 Dir.foreach("results/naming/") do |file|
-  if file =~ /(Lsa|Lda|Depspace)(Cluster3|Cluto|Autoclass|Baseline|Label)([^\.]*)\.report$/
+  if file =~ /(Lsa|Lda|Depspace)(Cluster3|Cluto|Autoclass|Baseline|Label|Cw)([^\.]*)\.report$/
     if `cat results/naming/#{file} | wc -l`.strip.to_i > 0
       cats[$2] ||= []
       cats[$2].push "results/naming/"+file
@@ -19,7 +19,7 @@ end
 workdir = Dir.getwd
 cats.each_pair do |method,files|
   STDERR.puts "Plotting #{files.map { |x| x.split("/").pop }.join(", ")} into #{method}..."
-  graph = Graph::NamingPlot.new(files,method)
+  graph = Graph::NamingBarPlot.new(files,method)
   graph.render
   Dir.chdir(workdir)
 end
