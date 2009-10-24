f4723716c3568e262febd44d527c09128932ebe8
Trevor Fountain <doches@gmail.com>
2009-10-24 19:10:16 +0100

    Added an interface for RemoteHash.finish

--- lib/DepspaceModel.rb
+++ lib/DepspaceModel.rb
@@ -7,12 +7,23 @@ class DepspaceModel < Model
     super
     
     @datapath = "data/depspace/"
-    @legacy_model = DependencySpace.new(@datapath + "wide_length.vectors",true)
-    #@model = RemoteHash.new("mcfadden",9998)
+    @model = nil
+    #@legacy_model = DependencySpace.new(@datapath + "wide_length.vectors",true)
+    @model = RemoteHash.new("mcfadden",9998)
   end
   
   def vector(word)
-    return @legacy_model.vector(word)
-    #return @model[word]
+    #return @legacy_model.vector(word)
+    str = @model[word]
+    begin
+      return str.split(" ").map { |x| x.to_f }
+    rescue # BAD IDEA!
+      return [0]
+    end
+  end
+  
+  # Close out RemoteHash, if using it.
+  def finish
+    @model.release if not @model.nil? and @model.is_a?(RemoteHash)
   end
 end
